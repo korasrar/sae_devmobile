e87b018eaa556b640db1d519ff79e26d5ab21661
Rousseau Esteban <o22400319@or-iut-i204-f03.iut45.univ-orleans.fr>
2026-02-02 16:31:39 +0100

    ajout du peuplement de la bd (généré pas IA), et ajout de la requete 1 (ville accéssible depuis paris)

---
+++ script_bd/ville_depuis_paris.sql
@@ -0,0 +1,7 @@
+SELECT VILLE.nom_ville
+FROM VOL
+JOIN AEROPORT ON VOL.id_aeroport_arrive = AEROPORT.id_aeroport
+JOIN VILLE ON AEROPORT.id_ville = VILLE.id_ville
+JOIN AEROPORT A2 ON VOL.id_aeroport_depart = A2.id_aeroport
+JOIN VILLE V2 ON A2.id_ville = V2.id_ville
+WHERE V2.nom_ville = 'Paris';
