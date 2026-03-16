6903b09213375b0296413cd95ac427a6bde63538
Rousseau Esteban <o22400319@or-iut-i102-f17.iut45.univ-orleans.fr>
2026-03-16 16:47:12 +0100

    beaucoup de chose car j'ai oublier de commit, v1 API, avec la possibilité de POST, GET, DELETE, PUT sur les tables

---
+++ API_REST_Alchemy/todo/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+def mkpath(p):
+    return os.path.normpath(
+        os.path.join(
+            os.path.dirname(__file__), p)
+    )
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///'+ mkpath('../vol.db'))
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
