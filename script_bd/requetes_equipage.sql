1fc86ca8bdc1daad13559779068b1dfa10eb0cc2
Esteban Rousseau <esteban.rousseau@etu.univ-orleans.fr>
2026-04-02 17:01:49 +0200

    f

--- script_bd/requetes_equipage.sql
+++ script_bd/requetes_equipage.sql
@@ -18,3 +18,27 @@ FROM VOL v,
 WHERE e.fonction = 'Pilote'
 GROUP BY e.nom
 ORDER BY e.nom;
+
+
+-- (c) Pour chaque vol, indiquer l'impact de chaque indice de qualité
+-- L'impact = valeur * poid
+SELECT 
+    v.NumVol,
+    i.Atype as indice_type,
+    i.valeur,
+    i.poid,
+    (i.valeur * i.poid) as impact
+FROM VOL v,
+     TABLE(v.IndicesQualite) i
+ORDER BY v.NumVol, i.Atype;
+
+
+-- (d) Pour chaque indice de qualité, calculer son impact moyen
+SELECT 
+    i.Atype as indice_type,
+    ROUND(AVG(i.valeur * i.poid), 2) as impact_moyen
+FROM VOL v,
+     TABLE(v.IndicesQualite) i
+GROUP BY i.Atype
+ORDER BY i.Atype;
+
